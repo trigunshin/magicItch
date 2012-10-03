f3d3de3d3377e75a10d84a37c6299b157745ac95
pcrane <patrick.crane@yellglobal.com>
2012-10-03 14:09:19 -0400

    Read rarities from file and insert to mongo.

---
+++ src/rarityExport.py
@@ -0,0 +1,72 @@
+import pymongo
+from pymongo import Connection
+from datetime import date
+import sys,csv,argparse,jsonpickle,math
+
+class scgImports(object):
+    def __init__(self, aDelimiter = ",", verbose=None):
+        self.verbose = verbose
+        self.delimiter = aDelimiter
+        self.setIndex = 0
+        self.nameIndex = 1
+        self.rarityIndex = 4
+
+    def getReader(self, file):
+        return csv.reader(file, delimiter=self.delimiter)
+
+    def getCardInfo(self, aFile, collection):
+        ret = []
+        with open(aFile, 'rb') as f:
+            reader = self.getReader(f)
+            for row in reader:
+                cardName = row[self.nameIndex]
+                cardSet = row[self.setIndex]
+                cardRarity = row[self.rarityIndex]
+                ret.append({"name":name,"set":set,"rarity":cardRarity,"date":self.datestring})
+                #collection.insert(val)
+        return ret
+
+if __name__ == '__main__':
+    verbose = False
+    delimiter = "\t"
+    fileSuffix = ".tsv"
+    fileName = None
+    fullFileDirectory = ""
+    
+    parser = argparse.ArgumentParser(description='Upload a scg Xsv file to the mongo db.')
+    parser.add_argument('-f', required=True, help="Directory to find the file in")
+    parser.add_argument('-n', required=False, help="File name to read data from")
+    parser.add_argument('-t', action='store_true', help="Denote a TSV file")
+    parser.add_argument('-c', action='store_true', help="Denote a CSV file")
+    
+    args = vars(parser.parse_args())
+    
+    if args['t']:
+        delimiter = "\t"
+        fileSuffix = ".tsv"
+    elif args['c']:
+        delimiter = ","
+        fileSuffix = ".csv"
+    if args['s']:
+        storeName = args['s']
+    if args['n'] != None:
+        fileName = args['n']
+    if args['f'] != None:
+        fullFileDirectory = args['f']
+    if fileName == None:
+        fileName = "scg_"+datestring+fileSuffix
+    fileToUse = fullFileDirectory + fileName
+    
+    print "Reading from:",fileToUse
+    
+    c = Connection()
+    db = c['cardInfo']
+    coll = db['cards']
+    
+    imp = scgImports(delimiter)
+    
+    listings = imp.getCardInfo(fileToUse, coll)
+    coll.insert(listings)
+    #for post in coll.find(dateQueryParam).limit(2).sort("name"):
+    #    print post
+    
