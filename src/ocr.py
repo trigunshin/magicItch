81f8ff120be61de3186fc095654ddb816a69cca0
Patrick Crane <trigunshin@gmail.com>
2013-01-11 18:13:49 -0500

    added validation method

--- src/ocr.py
+++ src/ocr.py
@@ -3,20 +3,47 @@ from pymongo import Connection
 from datetime import date
 import time, re, argparse, sys, csv, subprocess
 
-if __name__ == '__main__':
-    wdir = "../stest/"
-    sdir = "../stest/"
-    scriptName = "tess.sh"
-    print "working dir:",wdir
-    print "scriptdir:",sdir+scriptName
-    popen = subprocess.check_output(["/bin/bash", sdir+scriptName, wdir]).split('\n')[:-1]
-    triple = [line.split(' ') for line in popen]
-    for t in triple: print t
-    #print "result:",popen
-    """
-    for file in glob(wdir+"*.png"):
-        print file
-        #subprocess.call(script + [arg1])
-    #"""
-    
+def validateOCR(aResult):
+    ret = {}
+    check = {}
+    for i in range(len(aResult)):
+        #fail out if duplicate detected, OCR is inaccurate
+        if check[aResult[i]]: return None
+        else:ret[i] = aResult[i]
+    return ret
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Run OCR on downloaded sprite files.')
+    parser.add_argument('-v', action='store_true', help='Verbose flag')
+    parser.add_argument('-d', action='store_true', help='Debug flag')
+    parser.add_argument('-s', help="Sprite file directory.")
+    parser.add_argument('-f', help="OCR script location.")
+    args = vars(parser.parse_args())
+    
+    verbose = False
+    debug = False
+    ocrScript = "tess.sh"
+    spriteDir= "sprites/"
+    
+    c = Connection()
+    db = c['cardData']
+    coll = db['cards']
+    sprites = db['sprites']
+    
+    if args['v']:
+        verbose = args['v']
+    if args['d']:
+        debug = args['d']
+    if args['f'] != None:
+        ocrScript = args['f']
+    if args['s'] != None:
+        spriteDir = args['s']
+    
+    spriteDir = "sprites/"
+    popen = subprocess.check_output(["/bin/bash", ocrScript, spriteDir]).split('\n')[:-1]
+    triple = [line.split(' ') for line in popen]
+    for t in triple:
+        print "hash:", t[2]
+        print "ocr_top:",validateOCR(t[0])
+        print "ocr_bot:",validateOCR(t[1])
+    
