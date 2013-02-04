099dfb8b2ed2cbf85a51608f746df5eaf29ee88e
pcrane <patrick.crane@yellglobal.com>
2013-02-04 15:58:33 -0500

    updated to be fully integrated

--- src/tess.py
+++ src/tess.py
@@ -1,20 +1,51 @@
-import subprocess
-import glob
-from ocr import chooseResult
-from ocr import insertPeriod
-from ocr import validateOCR
+import subprocess, glob, argparse
+from pymongo import MongoClient
 
+def validateOCR(aResult,verbose=False):
+    ret = {}
+    check = {}
+    if verbose: print "checking",aResult
+    for i in range(len(aResult)):
+        #fail out if duplicate detected, OCR is inaccurate
+        if verbose:print "\tchecking idx",i,"w/val",aResult[i]
+        try:
+            if check[aResult[i]]:
+                if verbose: print "had result",aResult[i],"at",i," quitting"
+                #print check
+                return None
+        except KeyError,e:
+            check[aResult[i]] = True
+            ret[str(i)] = aResult[i]
+    return ret
+
+def insertPeriod(aString):
+    return aString[0:9] + '.' + aString[-1]
+
+def chooseResult(first, second, verbose=False):
+    #11 length dict should be correct, use that one
+    if len(first) == 11: return validateOCR(first, verbose)
+    elif len(second) == 11: return validateOCR(second, verbose)
+    
+    #try the first
+    if len(first) == 10:
+        ret = validateOCR(insertPeriod(first), verbose)
+        if ret is not None: return ret
+    #first had bad len or didn't convert, try second
+    if len(second) == 10:
+        return validateOCR(insertPeriod(second), verbose)
+    #nothing had length or converted
+    return None
 
 class ConvertCaller():
     def __init__(self):
         self.opts=[
+            'convert $PATH -despeckle -threshold 50% -auto-level -resize 638x364 -compress none $NAME.tiff',#effective
+            'convert $PATH -auto-level -resize 837x455 -compress none $NAME.tiff',#effective
             'convert $PATH -auto-level -resize 162x91 -compress none $NAME.tiff',
             'convert $PATH -auto-level -resize 243x137 -compress none $NAME.tiff',
             'convert $PATH -auto-level -resize 324x182 -compress none $NAME.tiff',
             'convert $PATH -auto-level -resize 486x273 -compress none $NAME.tiff',
             'convert $PATH -auto-level -resize 648x364 -compress none $NAME.tiff',
-            'convert $PATH -despeckle -threshold 50% -auto-level -resize 638x364 -compress none $NAME.tiff',#effective
-            'convert $PATH -auto-level -resize 837x455 -compress none $NAME.tiff',#effective
             'convert $PATH -auto-level -resize 1296x728 -compress none $NAME.tiff',
             'convert $PATH -auto-level -resize 1458x819 -compress none $NAME.tiff'
         ]
@@ -24,20 +55,37 @@ class ConvertCaller():
         for cur in self.opts:
             yield cur.replace(self.path,path).replace(self.name,name).split(' ')
     
-
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Run OCR with varying arguments over sprite files.')
+    parser.add_argument('-v', action='store_true', help='Verbose flag')
+    parser.add_argument('-d', action='store_true', help='Debug flag')
+    parser.add_argument('-s', help="Sprite file directory. Will break if path contains spaces.")
+    args = vars(parser.parse_args())
+    
+    verbose = False
+    debug = False
+    imageDir= "sprites/"
+    
+    c = MongoClient('localhost',27017)
+    db = c['cardData']
+    sprites = db['sprites']
+    
+    if args['v']:
+        verbose = args['v']
+    if args['d']:
+        debug = args['d']
+    if args['s'] != None:
+        imageDir = args['s']
+    
     conv = ConvertCaller()
-    i=0
-    imageDir = "../sprites/"
     for cur in glob.iglob(imageDir + '*.png'):
         print "cur path:",cur
         imageHash = cur.split('/')[-1].split('.')[0]
         print "hash",imageHash
         
         for convCall in conv.getNext(cur,imageHash):
-            #print "conv args:",convCall
-            rval = subprocess.call(convCall)
-            if not rval == 0:
+            convertResult = subprocess.call(convCall)
+            if not convertResult == 0:
                 #handle convert error
                 print "error on convert"
             
@@ -47,22 +95,13 @@ if __name__ == '__main__':
                 print "tess error"
                 break
             result = subprocess.check_output(['cat',imageHash+'.txt']).strip().split('\n')
-            #print "le text:",result
-            result = chooseResult(*result)
+            chosen = chooseResult(*result)
             print "\tchose:", result
-            #if i>0:break
-            #i+=1
             subprocess.check_output(['rm',imageHash+'.txt'])
             subprocess.check_output(['rm',imageHash+'.tiff'])
-            if result is not None:
-                print "done!"
-                #break
-            
-        break
-        #mv "$imageHash.png" "done"
-        #rm "$imageHash.txt"
-        
-        #popen = subprocess.check_output(["/bin/bash", ocrScript, spriteDir])
-        #for line in subprocess.check_output(["ls","-lah"]):
-        #    print line
-
+            if chosen is not None:
+                if debug is False:
+                    sprites.update({'hash':imageHash},{'$set':{'values':chosen}}, upsert=True)
+                    code = subprocess.call(["mv", spriteDir+imageHash+".png", spriteDir+"done/"])
+                    if not code == 0: print "error moving hashfile",spriteDir+imageHash+".png","to",spriteDir+"done/"
+                break
