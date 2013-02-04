7e754ca565cc29a73e9fc723a88a7547a971fa83
patrick <trigunshin@gmail.com>
2013-02-04 14:18:50 -0500

    executions run on proper parameters

--- src/tess.py
+++ src/tess.py
@@ -2,21 +2,25 @@ import subprocess
 import glob
 
 if __name__ == '__main__':
-    for cur in glob.iglob('*.png'):
-        imageHash = cur.split('.')[0]
-        
+    imageDir = "../sprites/"
+    for cur in glob.iglob(imageDir + '*.png'):
+        print "cur path:",cur
+        imageHash = cur.split('/')[-1].split('.')[0]
+        print "hash",imageHash
         #convert "$imageHash".png -auto-level -resize 648x364 -compress none "$imageHash".tiff
-        rval = subprocess.call(['convert',imageHash+'.png','-auto-level','-resize','648x364','-compress','none',imageHash+'.tiff'])
+        rval = subprocess.call(['convert',cur,'-auto-level','-resize','648x364','-compress','none',imageHash+'.tiff'])
         if not rval == 0:
             #handle convert error
             print "error on convert"
         
         #tesseract creates a $imageHash.txt output file
         #tesseract "$imageHash".tiff "$imageHash" mitch 1>/dev/null 2>&1
-        if not 0==subprocess.call(['tesseract', imageHash+'.tiff', imageHash, 'mitch']:
+        if not 0==subprocess.call(['tesseract', imageHash+'.tiff', imageHash, 'mitch']):
             #handle tesseract error
+            print "tess error"
+            break
         txt = subprocess.check_output(['cat',imageHash+'.txt'])
-        print "le text",txt
+        print "le text:\n",txt.strip().split('\n')
         break
         #subprocess.check_output(['paste','-s','-d','|',txt])
         #TEXT=`cat $imageHash.txt | paste -s -d '|'`
