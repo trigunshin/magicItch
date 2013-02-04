cc938fac8052e1b66160cb8b94afb2bac9b70d1b
patrick <trigunshin@gmail.com>
2013-02-04 15:17:58 -0500

    added convert args to run through permutations for each image

--- src/tess.py
+++ src/tess.py
@@ -1,30 +1,33 @@
 import subprocess
 import glob
-import ocr
+from ocr import chooseResult
+from ocr import insertPeriod
+from ocr import validateOCR
 
 
 class ConvertCaller():
     def __init__(self):
         self.opts=[
-            'convert $PATH -auto-level -resize 162x91 -compress none "$NAME".tiff',
-            'convert "$PATH -auto-level -resize 243x137 -compress none "$NAME".tiff'
-            'convert "$PATH -auto-level -resize 324x182 -compress none "$NAME".tiff'
-            'convert "$PATH -auto-level -resize 486x273 -compress none "$NAME".tiff'
-            'convert "$PATH -auto-level -resize 648x364 -compress none "$NAME".tiff'
-            'convert "$PATH -despeckle -threshold 50% -auto-level -resize 638x364 -compress none "$NAME".tiff'#effective
-            'convert "$PATH -auto-level -resize 837x455 -compress none "$NAME".tiff'#effective
-            'convert "$PATH -auto-level -resize 1296x728 -compress none "$NAME".tiff'
-            'convert "$PATH -auto-level -resize 1458x819 -compress none "$NAME".tiff'
+            'convert $PATH -auto-level -resize 162x91 -compress none $NAME.tiff',
+            'convert $PATH -auto-level -resize 243x137 -compress none $NAME.tiff',
+            'convert $PATH -auto-level -resize 324x182 -compress none $NAME.tiff',
+            'convert $PATH -auto-level -resize 486x273 -compress none $NAME.tiff',
+            'convert $PATH -auto-level -resize 648x364 -compress none $NAME.tiff',
+            'convert $PATH -despeckle -threshold 50% -auto-level -resize 638x364 -compress none $NAME.tiff',#effective
+            'convert $PATH -auto-level -resize 837x455 -compress none $NAME.tiff',#effective
+            'convert $PATH -auto-level -resize 1296x728 -compress none $NAME.tiff',
+            'convert $PATH -auto-level -resize 1458x819 -compress none $NAME.tiff'
         ]
         self.path="$PATH"
         self.name="$NAME"
-    def getNext(path,name):
+    def getNext(self,path,name):
         for cur in self.opts:
             yield cur.replace(self.path,path).replace(self.name,name).split(' ')
     
 
 if __name__ == '__main__':
     conv = ConvertCaller()
+    i=0
     imageDir = "../sprites/"
     for cur in glob.iglob(imageDir + '*.png'):
         print "cur path:",cur
@@ -32,23 +35,29 @@ if __name__ == '__main__':
         print "hash",imageHash
         
         for convCall in conv.getNext(cur,imageHash):
-            #convert "$imageHash".png -auto-level -resize 648x364 -compress none "$imageHash".tiff
-            rval = subprocess.call(['convert',cur,'-auto-level','-resize','648x364','-compress','none',imageHash+'.tiff'])
+            #print "conv args:",convCall
+            rval = subprocess.call(convCall)
             if not rval == 0:
                 #handle convert error
                 print "error on convert"
             
             #tesseract creates a $imageHash.txt output file
-            #tesseract "$imageHash".tiff "$imageHash" mitch 1>/dev/null 2>&1
             if not 0==subprocess.call(['tesseract', imageHash+'.tiff', imageHash, 'mitch']):
                 #handle tesseract error
                 print "tess error"
                 break
             result = subprocess.check_output(['cat',imageHash+'.txt']).strip().split('\n')
-            print "le text:\n",result
+            #print "le text:",result
             result = chooseResult(*result)
             print "\tchose:", result
-            break
+            #if i>0:break
+            #i+=1
+            subprocess.check_output(['rm',imageHash+'.txt'])
+            subprocess.check_output(['rm',imageHash+'.tiff'])
+            if result is not None:
+                print "done!"
+                #break
+            
         break
         #mv "$imageHash.png" "done"
         #rm "$imageHash.txt"
