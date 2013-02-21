36e2fa0b1df856515072af291d7f901a91192587
patrick <trigunshin@gmail.com>
2013-02-20 20:24:49 -0500

    Added strip back in before replace.

--- src/processSprites.py
+++ src/processSprites.py
@@ -100,7 +100,7 @@ if __name__ == '__main__':
                 #handle tesseract error
                 print "tess error"
                 break
-            result = subprocess.check_output(['cat',imageHash+'.txt']).replace(' ','').split('\n')
+            result = subprocess.check_output(['cat',imageHash+'.txt']).strip().replace(' ','').split('\n')
             print "\tresult:",result
             chosen = chooseResult(*result)
             print "\tchose:", chosen
