0541e57b10c2dbc4341145f2526bbb968c16e6dd
patrick <trigunshin@gmail.com>
2013-02-28 23:30:49 -0500

    updated cron file to reflect new hierarchy. still has a script error somewhere...

--- cron.sh
+++ cron.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
-DEVDIR="/home/pcrane/dev/"
-PYDEVDIR=$DEVDIR"python/"
-ITCHDIR=$PYDEVDIR"magicItch/"
-SRCDIR=$ITCHDIR"src/"
-INFODIR=$ITCHDIR"info/"
-DAILYDIR=$ITCHDIR"daily/"
 HOMEDIR="/home/pcrane/"
-LOGDIR=$HOMEDIR"logs/"
+DEVDIR=$HOMEDIR"dev/"
+ITCHDIR=$DEVDIR"magicItch/"
+SRCDIR=$ITCHDIR"src/"
+SCG_DATA_DIR=$ITCHDIR"scg_data/"
+SPRITE_DIR=$ITCHDIR"sprites/"
+LOGDIR=$HOMEDIR"logs/magicItch/"
 DATE=$(date -I)
 YEST=$(date -I -d '1 day ago')
 
-python $SRCDIR"magicTraders.py" -f $INFODIR >> $LOGDIR"log"$DATE".log" \
-    && python $SRCDIR"mongoExport.py" -f $INFODIR -d $DATE -n "mtgtrdr_"$DATE".tsv" -s "Magic Traders" >>$LOGDIR"log"$DATE".log" \
-    && python $SRCDIR"mongoPriceReport.py" -s $YEST -e $DATE -o $DAILYDIR -c -n mtgtrdrDaily$DATE".txt" -r "Magic Traders" >>$LOGDIR"log"$DATE".log" \
-    && python $SRCDIR"mongoPriceReport.py" -s $YEST -e $DATE -o $DAILYDIR -n mtgtrdrDaily$DATE".csv" -r "Magic Traders" >>$LOGDIR"log"$DATE".log"
-python $ITCHDIR"allinMain.py" -f $INFODIR >>$LOGDIR"log"$DATE".log" \
-    && python $SRCDIR"mongoExport.py" -f $INFODIR -d $DATE >>$LOGDIR"log"$DATE".log" \
-    && python $SRCDIR"mongoPriceReport.py" -s $YEST -e $DATE -o $DAILYDIR -c -n scgDaily$DATE".txt" >>$LOGDIR"log"$DATE".log" \
-    && python $SRCDIR"mongoPriceReport.py" -s $YEST -e $DATE -o $DAILYDIR -n scgDaily$DATE".csv" >>$LOGDIR"log"$DATE".log"
-python $SRCDIR"sendMuxGmail.py" -d $DAILYDIR >>$LOGDIR"log"$DATE".log"
-#python $SRCDIR"sendGmail.py" -f $DAILYDIR"scgDaily"$DATE".txt" -f $DAILYDIR"mtgtrdrDaily"$DATE".txt" >>$LOGDIR"log"$DATE".log"
+python $SRCDIR"sprites.py" -f $SCG_DATA_DIR -s $SPRITE_DIR -m $SRCDIR"ocr_map.csv" &> $LOGDIR"log_"$DATE".log"
+python $SRCDIR"processSprites.py" -s $SPRITE_DIR &> $LOGDIR"log_"$DATE".log"
